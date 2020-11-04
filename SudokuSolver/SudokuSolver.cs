79aacd3001b998dceb3a0c52782530345b05471d
Nikesh Bhagalia <nikeshbhagalia@hotmail.com>
2020-11-04 17:14:04 +1300

    Added solve logic

--- SudokuSolver/SudokuSolver.cs
+++ SudokuSolver/SudokuSolver.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.Windows.Forms;
 
 namespace SudokuSolver
@@ -29,20 +28,72 @@ namespace SudokuSolver
                 }
             }
 
-            while (sudoku.Cast<short>().ToList().Any(s => s != 0))
+            var solved = SolveSudoku(sudoku, 0, 0);
+
+            if (solved)
             {
                 for (var i = 0; i < SudokuSize; i++)
                 {
                     for (var j = 0; j < SudokuSize; j++)
                     {
-                        var square = sudoku[i, j];
-                        if (square != 0)
-                        {
-
-                        }
+                        _sudokuPanel.GetControlFromPosition(i, j).Text = sudoku[i, j].ToString();
                     }
                 }
             }
+        }
+
+        private bool SolveSudoku(short[,] sudoku, int row, int column)
+        {
+            if (row == SudokuSize - 1 && column == SudokuSize)
+                return true;
+
+            if (column == SudokuSize)
+            {
+                row++;
+                column = 0;
+            }
+
+            if (sudoku[row,column] != 0)
+            {
+                return SolveSudoku(sudoku, row, column + 1);
+            }
+                
+
+            for (var number = 1; number <= SudokuSize; number++)
+            {
+                if (isValid(sudoku, row, column, number))
+                {
+                    sudoku[row,column] = (short)number;
+
+                    if (SolveSudoku(sudoku, row, column + 1))
+                        return true;
+                }
+
+                sudoku[row,column] = 0;
+            }
+
+            return false;
+        }
+
+        private bool isValid(short[,] sudoku, int row, int column, int number)
+        {
+            for (var x = 0; x <= 8; x++)
+                if (sudoku[row,x] == number)
+                    return false;
+
+            for (int x = 0; x <= 8; x++)
+                if (sudoku[x,column] == number)
+                    return false;
+
+            var startRow = row - row % 3;
+            var startCol = column - column % 3;
+
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    if (sudoku[i + startRow,j + startCol] == number)
+                        return false;
+
+            return true;
         }
     }
 }
