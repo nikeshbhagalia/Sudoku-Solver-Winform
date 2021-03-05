1c37e062b513dad49dc74a5e2b9724725137fee2
nikeshbhagalia <14082394+nikeshbhagalia@users.noreply.github.com>
2021-03-06 12:41:50 +1300

    Added curly braces to for loop

--- SudokuSolver/SudokuSolver.cs
+++ SudokuSolver/SudokuSolver.cs
@@ -103,8 +103,10 @@ namespace SudokuSolver
             for (var i = 0; i < SquareSize; i++)
             {
                 for (var j = 0; j < SquareSize; j++)
+                {
                     if (sudoku[i + startRow,j + startCol] is number)
                         return false;
+                }
             }
 
             return true;
